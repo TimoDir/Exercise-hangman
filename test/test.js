ca0fde7d30f951f8ce34ffc3cc0fee296d0ba1e7
TimD <timothee.diroian@gmail.com>
2022-05-26 13:05:41 +0200

    Adjustement in the test unit to controle the input even when upper case - in reveal it -

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ var mask = new Array(secretLength).fill('_');
 var mistakeCount = 0;
 
 describe('guess', function(){
-    const goodCharacter = 'a';
+    const goodCharacter = 'w';
     const badCharacter = 'd';
     const output = guess();
 
@@ -19,15 +19,17 @@ describe('guess', function(){
       });
 
     it('Should return a string', function(){
-        assert.strictEqual(typeof(output), 'string', "The function didn't return a string")
+        const expectedResult = 'string';
+        assert.strictEqual(typeof(output), expectedResult, "The function didn't return a string")
     });
     it('Should return a mask string with the same length that the secret word.', function(){
-        assert.strictEqual(output.length, secret.length, "The string return by the function is not the same length than the secret word.")
+        const expectedResult = secret.length;
+        assert.strictEqual(output.length, expectedResult, "The string return by the function is not the same length than the secret word.")
     });
     it('Should change the mask and reveal the letter of secret when a good letter is pass as an argument.', function(){
-        const expectedResult = goodCharacter;
+        const expectedResult = goodCharacter.toLowerCase();
         // Should return an array with the good character with the filter method + accessing it by typing the first index [0]
-        const controle = guess(goodCharacter).split('').filter(letter => letter.toLowerCase() == goodCharacter)[0];
+        const controle = guess(goodCharacter).split('').filter(letter => letter.toLowerCase() == goodCharacter.toLowerCase())[0];
         assert.strictEqual(controle, expectedResult, "The function didn't reveal the mask when the argument is a good character.")
     });
     it("Should return 'GAME OVER!' when 6 or more mistake was made.", function(){
