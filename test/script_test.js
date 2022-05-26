18d9e28b7d0608d5bf989ed8c66c85282647812a
TimD <timothee.diroian@gmail.com>
2022-05-26 13:01:43 +0200

    Creation of the script_test copy of the script for testing + comment adustement

--- test/script_test.js
+++ test/script_test.js
@@ -34,22 +34,5 @@ function guess(character) {
   } else return mask.join('');
 };
 
-
-// output test
-const log = msg => {
-  const divElement = document.createElement("div");
-  divElement.innerText = msg;
-  document.getElementById("log").append(divElement);
-};
-// Try uncommenting this
-
-log(guess("u"))
-log(guess("a"))
-log(guess("w"))
-log(guess("e"))
-log(guess("w"))
-log(guess("g"))
-log(guess("t"))
-log(guess("d"))
-log(guess("r"))
-log(guess("s"))
+// export the function to the test unit
+module.exports = guess;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var guess = require('../src/script')
+var guess = require('./script_test')
 
 // Creation of the mask and mistakeCount outside the test unit too store the result
 const secret = 'wargaming'
@@ -24,18 +24,18 @@ describe('guess', function(){
     it('Should return a mask string with the same length that the secret word.', function(){
         assert.strictEqual(output.length, secret.length, "The string return by the function is not the same length than the secret word.")
     });
-    it('Should change the mask and reveale the letter of secret when a good letter is pass as an argument.', function(){
+    it('Should change the mask and reveal the letter of secret when a good letter is pass as an argument.', function(){
         const expectedResult = goodCharacter;
         // Should return an array with the good character with the filter method + accessing it by typing the first index [0]
         const controle = guess(goodCharacter).split('').filter(letter => letter.toLowerCase() == goodCharacter)[0];
         assert.strictEqual(controle, expectedResult, "The function didn't reveal the mask when the argument is a good character.")
     });
-    it("Should return 'GAME OVER!' when 6 or more mistake was made", function(){
+    it("Should return 'GAME OVER!' when 6 or more mistake was made.", function(){
         const expectedResult = 'GAME OVER!';
         for(let i=0; i < 8; i++){
             guess(badCharacter);
         };
-        const controle= guess(goodCharacter);
+        const controle= guess(badCharacter);
         assert.strictEqual(controle, expectedResult, "The function didn't return 'GAME OVER!' when 6 and more mistake was made.")
     });
-})
+});
