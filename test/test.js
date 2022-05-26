c3061a912ae17eba05ede6b4b78a47b3ef3844a2
TimD <timothee.diroian@gmail.com>
2022-05-26 10:13:23 +0200

    Adding more test to the test unit until one failed

--- test/test.js
+++ test/test.js
@@ -3,10 +3,18 @@ var guess = require('../src/script')
 
 describe('guess', function(){
     const secret = 'wargaming';
-    const goodCaracter = 'w';
-    const badCaracter = 'd';
+    const goodCharacter = 'a';
+    const badCharacter = 'd';
     const output = guess();
+    it('Should return a string', function(){
+        assert.strictEqual(typeof(output), 'string', "The function didn't return a string")
+    });
     it('Should return a mask string with the same length that the secret word.', function(){
         assert.strictEqual(output.length, secret.length, "The string return by the function is not the same length than the secret word.")
+    });
+    it('Should change the mask and reveale the letter of secret when a good letter is pass as an argument.', function(){
+        var expetResult = secret.split('').filter(letter => letter.toLowerCase() == goodCharacter);
+        var controle = guess(goodCharacter).split('').filter(letter => letter.toLowerCase() == goodCharacter);
+        assert.strictEqual(controle, expetResult, "The function didn't reveal the mask when the argument is a good character.")
     })
 })
